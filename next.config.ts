aa72c01633503271a302cdff7d4f351a9d09e4a9
irgi <irgiaditpratama@gmail.com>
2025-10-25 22:49:26 +0700

    pro page & pricing table added

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,20 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol:"https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+    ],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
